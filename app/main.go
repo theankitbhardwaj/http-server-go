69d5841bcd202ddd78f65781192f29b7393dc152
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2025-08-31 18:19:10 +0530

    Initial Commit: Accepting connection on port

---
+++ app/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	fmt.Println("Logs from HTTP Server")
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	conn, err := l.Accept()
+
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/theankitbhardwaj/http-server-go
+
+go 1.24.6
